6f0b54368eaf805d678263860a3114db37efc4ab
javie <javiergv2004@gmail.com>
2024-02-01 19:16:36 +0100

    CREACION PANTALLA NEW

--- app/src/main/java/com/example/nightendar/NewActivity.kt
+++ app/src/main/java/com/example/nightendar/NewActivity.kt
@@ -1,14 +1,21 @@
 package com.example.nightendar
 
+import android.content.ContentValues
 import android.content.Intent
+import android.health.connect.datatypes.BloodPressureRecord.BloodPressureMeasurementLocation
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.RatingBar
 import androidx.appcompat.app.AppCompatActivity
 
 class NewActivity : AppCompatActivity() {
 
     private var username: String? = null
     private var password: String? = null
+    private lateinit var editTextlocalName: EditText
+    private lateinit var editTextLocalLocation: EditText
+    private lateinit var rating: RatingBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,24 +25,28 @@ class NewActivity : AppCompatActivity() {
         val imageView3: ImageView = findViewById(R.id.imageView3)
         val imageView6: ImageView= findViewById(R.id.imageView6)
         val imageView9: ImageView= findViewById(R.id.imageView9)
-
-
+        val imageView: ImageView = findViewById(R.id.imageView)
+        editTextlocalName = findViewById(R.id.editTextLocalName)
+        editTextLocalLocation = findViewById(R.id.editTextLocalLocation)
+        rating=findViewById(R.id.ratingBar)
         // Obtén el username y el password del Intent
         val intent: Intent = intent
         username = intent.getStringExtra("username")
         password = intent.getStringExtra("password")
 
-        // Configura el OnClickListener para imageView3 (volver a MainActivity)
         imageView3.setOnClickListener {
-            // Llama a la función para volver a la actividad principal
             volverAMain()
         }
-
-        // Configura el OnClickListener para imageView6
         imageView6.setOnClickListener {
-            // Llama a la función para cambiar a la actividad de configuración
             guardarEstancia()
         }
+        imageView.setOnClickListener {
+            abrirConfiguracion()
+        }
+        imageView9.setOnClickListener {
+            abrirUser()
+        }
+
     }
 
     // Función para volver a la actividad principal
@@ -57,6 +68,18 @@ class NewActivity : AppCompatActivity() {
         finish()
     }
     private fun guardarEstancia() {
+        var localName=editTextlocalName.text.toString()
+        var localUbi=editTextLocalLocation.text.toString()
+        var localRating=rating.getRating().toInt()
+        val admin = BaseDatosApp(this,"bdnightendar",null,1)
+        val bd = admin.writableDatabase
+        val reg = ContentValues()
+
+        reg.put("NAME",localName)
+        reg.put("UBICACION",localUbi)
+        reg.put("RATING",localRating)
+        bd.insert("LOCAL", null, reg)
+        bd.close()
         val intent = Intent(this, UserActivity::class.java)
         // Pasa el nombre de usuario y la contraseña como extras
         intent.putExtra("username", username)
@@ -64,5 +87,13 @@ class NewActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+    private fun abrirConfiguracion() {
+        val intent = Intent(this, SettingsActivity::class.java)
+        intent.putExtra("username", username)
+        intent.putExtra("password", password)
+        startActivity(intent)
+        finish()
+    }
+
 
 }
