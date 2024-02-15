49917331abcded85f8e9b8e15ad007175bc19635
TonyPeMo <tonypm2002@gmail.com>
2024-02-15 19:26:47 +0100

    Cambio de checkbox a switch. Falta añadir funcionabilidad

--- app/src/main/java/com/example/nightendar/SettingsActivity.kt
+++ app/src/main/java/com/example/nightendar/SettingsActivity.kt
@@ -9,18 +9,12 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
 
-    private lateinit var sharedPreferences: SharedPreferences
     private var username: String? = null
     private var password: String? = null
-    private lateinit var opcionModoOscuro: CheckBox
-    private lateinit var opcionRecomendaciones: CheckBox
-    private lateinit var opcionUbicacion: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-
-        sharedPreferences = getSharedPreferences("checkbox_state", MODE_PRIVATE)
 
         // Obtén la referencia de la imagen
         val imageView3: ImageView = findViewById(R.id.imageView3)
@@ -31,29 +25,6 @@ class SettingsActivity : AppCompatActivity() {
         username = intent.getStringExtra("username")
         password = intent.getStringExtra("password")
 
-        // Obtener referencias de las CheckBox
-        opcionModoOscuro = findViewById(R.id.opcion_modo_oscuro)
-        opcionRecomendaciones = findViewById(R.id.opcion_recomendaciones)
-        opcionUbicacion = findViewById(R.id.opcion_ubicacion)
-
-        // Restaurar estados de las CheckBox
-        opcionModoOscuro.isChecked = sharedPreferences.getBoolean("modo_oscuro", true)
-        opcionRecomendaciones.isChecked = sharedPreferences.getBoolean("recomendaciones", true)
-        opcionUbicacion.isChecked = sharedPreferences.getBoolean("ubicacion", true)        // Configura el OnClickListener para imageView3 (volver a MainActivity)
-
-        // Configurar Listener para guardar cambios en SharedPreferences
-        opcionModoOscuro.setOnCheckedChangeListener { _, isChecked ->
-            sharedPreferences.edit().putBoolean("modo_oscuro", isChecked).apply()
-        }
-
-        opcionRecomendaciones.setOnCheckedChangeListener { _, isChecked ->
-            sharedPreferences.edit().putBoolean("recomendaciones", isChecked).apply()
-        }
-
-        opcionUbicacion.setOnCheckedChangeListener { _, isChecked ->
-            sharedPreferences.edit().putBoolean("ubicacion", isChecked).apply()
-        }
-        
         imageView3.setOnClickListener {
             // Llama a la función para volver a la actividad principal
             volverAMain()
